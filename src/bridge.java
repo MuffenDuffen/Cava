3526e4805aa2b6f3ee773e88752b5a1f2f7966bd
MuffenDuffen <50796186+MuffenDuffen@users.noreply.github.com>
2021-10-29 13:21:29 +0200

    worked a lot, gonna redo

--- src/bridge.java
+++
@@ -1,3 +0,0 @@
-public class bridge {
-    public native void Log();
-}
---
+++ src/bridge/bridge.java
@@ -0,0 +1,6 @@
+package bridge;
+
+public class bridge {
+    public static native void Log();
+    public static native void Hello();
+}
